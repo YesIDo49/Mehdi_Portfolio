a44e992a2c0cc5dda406293abcddbac752122023
YesIDo49 <mehdialsidcheikh@gmail.com>
2022-01-20 23:53:20 +0100

    beginning grow animation

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ document.addEventListener('mousemove', e=>{
   cursorFill.setAttribute("style", "top:" + (e.pageY -10) +"px; left:" + (e.pageX-10)+"px");
 
 })
+
+
+//Read more
+
+function growDiv() {
+ var growDiv = document.querySelector('.grow');
+ if (growDiv.clientHeight) {
+  growDiv.style.height = 0;
+ } else {
+  var wrapper = document.querySelector('.measuringWrapper');
+  growDiv.style.height = wrapper.clientHeight + "px";
+ }
+ document.getElementById("more-button").value = document.getElementById("more-button").value == 'Read more' ? 'Read less' : 'Read more';
+}
